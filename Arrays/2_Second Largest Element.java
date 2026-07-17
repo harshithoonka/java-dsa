6891bc05cfbf9c28039ea15b94d0ce8c0375f89a
Harshit Hoonka <harshithoonka22@gmail.com>
2026-07-17 23:00:09 +0530

    Day 002: Solved Second Largest Element in an Array

---
+++ Arrays/2_Second Largest Element.java
@@ -0,0 +1,32 @@
+public class SecondLargestElement {
+
+    public static int secondLargest(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            return -1;
+        }
+
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 35, 1, 10, 34, 1};
+
+        System.out.println(secondLargest(arr));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# java-dsa
-Daily Java DSA practice with clean solutions and optimized approaches.
+# Java DSA 🚀
+
+Welcome to my Java DSA repository.
+
+This repository contains my daily Java Data Structures and Algorithms practice for improve problem-solving skills. 
+
+## Topics
+
+- Arrays
+
+## Goal
+
+- Solve DSA problems consistently.
+- Improve problem-solving skills.
